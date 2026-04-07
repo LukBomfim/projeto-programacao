9f7a5bbe4e60632f905add4881a78c52f0084657
lucasmizaelsoares012-png <lucasmizaelsoares012@gmail.com>
2026-04-07 00:16:28 -0300

    Protótipo

--- app.py
+++ app.py
@@ -1,50 +1,52 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 import requests
 
 app = Flask(__name__)
 
-API_KEY = "eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6ImY4NmQ5OTU2OWQ0NzRiZGNiNDk2NzRmMDY0Y2EyYWQ2IiwiaCI6Im11cm11cjY0In0="
+
+ORS_KEY = 'eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6ImY4NmQ5OTU2OWQ0NzRiZGNiNDk2NzRmMDY0Y2EyYWQ2IiwiaCI6Im11cm11cjY0In0='
+
+MOTORISTAS = {
+    "carlos": {"nome": "Carlos (Uno)", "coords": [-35.714, -9.666]}, #ponta verde
+    "ana": {"nome": "Ana (Onix)", "coords": [-35.761, -9.582]}, #santa lucia
+    "bruno": {"nome": "Bruno (Gol)", "coords": [-35.729, -9.555]} #biu
+}
+
+
+FACULDADE_COORDS = [-35.703, -9.633] #unima
 
 @app.route("/")
 def home():
-    alunoA = [-9.666, -35.735]
-    alunoB = [-9.670, -35.750]
-    faculdade = [-9.648, -35.708]
+    return render_template("index.html")
 
-    distA = ((alunoA[0]-faculdade[0])**2 + (alunoA[1]-faculdade[1])**2)**0.5
-    distB = ((alunoB[0]-faculdade[0])**2 + (alunoB[1]-faculdade[1])**2)**0.5
+@app.route("/gerar_rota", methods=['POST'])
+def gerar_rota():
+    dados = request.json
+    moto_id = dados.get('motorista')
+    lat_p = float(dados.get('lat'))
+    lng_p = float(dados.get('lng'))
 
-    if distA > distB:
-        motorista, passageiro = alunoA, alunoB
-        msg = "Motorista A busca o Aluno B"
-    else:
-        motorista, passageiro = alunoB, alunoA
-        msg = "Motorista B busca o Aluno A"
+    
+    ponto_motorista = MOTORISTAS[moto_id]['coords']
+    ponto_embarque = [lng_p, lat_p]
+    ponto_destino = FACULDADE_COORDS
+
+    
+    corpo = {
+        "coordinates": [ponto_motorista, ponto_embarque, ponto_destino]
+    }
 
     url = "https://api.openrouteservice.org/v2/directions/driving-car/geojson"
     headers = {
-        "Authorization": API_KEY,
-        "Content-Type": "application/json"
-    }
-    body = {
-        "coordinates": [
-            [motorista[1], motorista[0]], 
-            [passageiro[1], passageiro[0]], 
-            [faculdade[1], faculdade[0]]
-        ]
+        'Authorization': ORS_KEY,
+        'Content-Type': 'application/json'
     }
 
-    response = requests.post(url, json=body, headers=headers)
-    dados_rota = response.json()
-
-    return render_template(
-        "index.html", 
-        rota_geojson=dados_rota, 
-        mensagem=msg,
-        pontoA=alunoA,
-        pontoB=alunoB,
-        faculdade=faculdade
-    )
+    try:
+        r = requests.post(url, json=corpo, headers=headers)
+        return jsonify(r.json())
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
     app.run(debug=True)
