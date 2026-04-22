125b86418c266049c17694462761d7d25eabd40d
LukBomfim <lucasbomfim2801@gmail.com>
2026-04-22 00:54:37 -0300

    criação do cadastro.html

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__, template_folder='frontend/templates', static_folder='front
 def homepage():
     return render_template('index.html')
 
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
